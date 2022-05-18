529d89c9d4dc5181657d78bbcaecaf3b179eb113
mouna33 <mounacamelia@yahoo.com>
2022-05-18 23:48:10 +0100

    mouna2 commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World, it's me");
-	System.out.println("Hello test");
+	System.out.println("Hello");
     }
 
 }
